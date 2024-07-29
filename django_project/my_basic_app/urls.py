4a63d0d64cb4eb6b43043386894e3a241e0ec83b
ahmedcurious <ahmed_naseem1@outlook.com>
2024-07-29 15:28:20 +0500

    Seventh Commit: Worked with Django forms with csrf tokens

--- django_project/my_basic_app/urls.py
+++ django_project/my_basic_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.app_basics, name='app_basics'),
-    path('<int:chai_id>/', views.chai_details, name='chai_details')
+    path('<int:chai_id>/', views.chai_details, name='chai_details'),
+    path('chai_stores/', views.chai_store_view, name='chai_store_view')
 ]
--- django_project/my_basic_app/views.py
+++ django_project/my_basic_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import ChaiVariety
+from .models import ChaiVariety, Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarietyForm
 
 # Create your views here.
 
@@ -13,3 +14,15 @@ def app_basics(request):
 def chai_details(request, chai_id: int):
     chai = get_object_or_404(ChaiVariety, pk=chai_id)
     return render(request, 'my_basic_app/chai_detail.html', {'chai': chai})
+
+
+def chai_store_view(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarietyForm(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(chai_varieties=chai_variety)
+    else:
+        form = ChaiVarietyForm()
+    return render(request, 'my_basic_app/chai_stores.html', {'stores': stores, 'form': form})
